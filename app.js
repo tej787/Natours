c33970704b6c9b762b3d61de870e431786e4dbaf
Tej Tandel <tejtandel2809@gmail.com>
2024-03-24 14:37:47 +0530

    Done

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const viewRouter = require('./routes/viewRoutes');
 // Start express app
 const app = express();
 
-// app.enable('trust proxy');
+//app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
